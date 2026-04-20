51f7bb0272cc96dc3be60c452625caa0566d8e8d
crimolf <alupo662299@gmail.com>
2026-04-20 23:35:41 +0200

    Palindrome

---
+++ src/main/java/leetcode/editor/en/p242/Valid Anagram.java
@@ -0,0 +1,44 @@
+package leetcode.editor.en.p242;
+
+//Given two strings s and t, return true if t is an anagram of s, and false 
+//otherwise. 
+//
+// 
+// Example 1: 
+//
+// 
+// Input: s = "anagram", t = "nagaram" 
+// 
+//
+// Output: true 
+//
+// Example 2: 
+//
+// 
+// Input: s = "rat", t = "car" 
+// 
+//
+// Output: false 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s and t consist of lowercase English letters. 
+// 
+//
+// 
+// Follow up: What if the inputs contain Unicode characters? How would you 
+//adapt your solution to such a case? 
+//
+// Related Topics Hash Table String Sorting 👍 14372 👎 480
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
